6b1c6477ac7e7ef6cb097d505d41d9ab7fa80aaa
jasminimumeffort <jsnbsbigsave@gmail.com>
2022-10-28 16:54:39 +0100

    hey this carousel is ugly but it works

--- React/react-hackathon/src/Components/Home.jsx
+++ React/react-hackathon/src/Components/Home.jsx
@@ -3,17 +3,22 @@ import CreateModal from "./CreateModal";
 import { Carousel, Toast } from 'react-bootstrap';
 import AlbumCarousel from "./AlbumCarousel";
 import {useState} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home({handleShowCreate}) {
     const Carou = () => {
         return(
+            //thhis is ugly but it works pending a resize https://pbs.twimg.com/media/FbbloNbUsAAeO3-.jpg
             <Carousel className = "Carousel" variant = 'dark'>
-            <Carousel.item>
-                <img className = "D-BLOCK W-100" src = "https://pbs.twimg.com/media/FbbloNbUsAAeO3-.jpg"/>
-            </Carousel.item>
-            <Carousel.item>
-                <img className = "D-BLOCK W-100" src = "https://upload.wikimedia.org/wikipedia/en/5/5b/Fearless_%28Taylor%27s_Version%29_%282021_album_cover%29_by_Taylor_Swift.png"/>
-            </Carousel.item>
+            <Carousel.Item>
+                <img className = "imgCarousel" src = "https://upload.wikimedia.org/wikipedia/en/thumb/4/47/Taylor_Swift_-_Red_%28Taylor%27s_Version%29.png/220px-Taylor_Swift_-_Red_%28Taylor%27s_Version%29.png"/>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className = "imgCarousel" src = "https://upload.wikimedia.org/wikipedia/en/5/5b/Fearless_%28Taylor%27s_Version%29_%282021_album_cover%29_by_Taylor_Swift.png"/>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img className = "imgCarousel" src = "https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Midnights_-_Taylor_Swift.png/220px-Midnights_-_Taylor_Swift.png"/>
+            </Carousel.Item>
             </Carousel>
         )
     }
@@ -33,7 +38,7 @@ function Home({handleShowCreate}) {
             </Toast.Header>
             <Toast.Body>Welcome to my silly little album database</Toast.Body>
             </Toast> */}
-            {/* <Carou/> */}
+            <Carou/>
 
             <button type="submit"><input className = "btn btn primary" type = "button" value ="Create Album" onClick={() => handleShowCreate({})}/></button>
         </div>
